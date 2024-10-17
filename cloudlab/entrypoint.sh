59a594557da3dd09e38d4f0d4f8155d6ae674be6
Scott Taylor <scott@ipv6tech.net>
2024-10-17 08:36:38 -0400

    additional tweaks to dockerfile, rename entrypoint script

--- cloudlab/entrypoint.sh
+++ cloudlab/entrypoint.sh
@@ -3,4 +3,5 @@ curl -L -O https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/insta
 chmod u+x install.sh
 ./install.sh --accept-all-defaults
 exec -l $SHELL
+rm install.sh entrypoint.sh
 mkdir -p ~/.ssh && cd ~/.ssh && ssh-keygen -f one-ring -N ''
