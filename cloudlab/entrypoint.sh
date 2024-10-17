68db3f587506a3f2e8b5e9fa93ec7944fcb0f204
Scott Taylor <scott@ipv6tech.net>
2024-10-17 14:07:06 -0400

    rebuild prebuilt container and devcontainer

--- cloudlab/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -L -O https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh
-chmod u+x install.sh
-./install.sh --accept-all-defaults
-exec -l $SHELL
-rm install.sh entrypoint.sh
-mkdir -p ~/.ssh && cd ~/.ssh && ssh-keygen -f one-ring -N ''
